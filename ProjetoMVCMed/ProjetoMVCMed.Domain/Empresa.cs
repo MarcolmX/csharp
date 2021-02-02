0b669fddd4aa3b3dbed52cf1e96eea5a798165cd
Marco Pacheco <60358284+MarcolmX@users.noreply.github.com>
2021-02-02 17:08:19 -0300

    Add files via upload

---
+++ ProjetoMVCMed/ProjetoMVCMed.Domain/Empresa.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoMVCMed.Domain
+{
+    public class Empresa
+    {
+        #region Propriedades
+
+        public Guid IdEmpresa           { get; set; }
+        public string RazaoSocial       { get; set; }
+        public string CNPJ              { get; set; }
+        public string Telefone          { get; set; }
+        public string Email             { get; set; }
+
+        #endregion
+
+        #region Relacionamentos
+        public List<Produto> Produtos   { get; set; }
+        public List<Solicitante> Solicitantes { get; set; }
+        #endregion
+    }
+}
